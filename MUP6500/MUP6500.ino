1cd6b8cec347842134d5a0101475f594b15150cf
Femke <femkedezee123@gmail.com>
2025-05-27 10:39:05 +0200

    MPU6500_v1
    Zonder integratie

---
+++ MUP6500/MUP6500.ino
@@ -0,0 +1,67 @@
+/***************************************************************************
+* Example sketch for the MPU6500_WE library
+*
+* This sketch shows how to get acceleration, gyroscocope and temperature 
+* data from the MPU6500. In essence, the difference to the MPU9250 is the
+* missing magnetometer. The shall only show how to "translate" all other 
+* MPU9250 example sketches for use of the MPU6500
+* 
+* For further information visit my blog:
+*
+* https://wolles-elektronikkiste.de/mpu9250-9-achsen-sensormodul-teil-1  (German)
+* https://wolles-elektronikkiste.de/en/mpu9250-9-axis-sensor-module-part-1  (English)
+* 
+***************************************************************************/
+
+#include <MPU6500_WE.h>
+#include <Wire.h>
+#define MPU6500_ADDR 0x68
+
+/* There are several ways to create your MPU6500 object:
+ * MPU6500_WE myMPU6500 = MPU6500_WE()              -> uses Wire / I2C Address = 0x68
+ * MPU6500_WE myMPU6500 = MPU6500_WE(MPU6500_ADDR)  -> uses Wire / MPU6500_ADDR
+ * MPU6500_WE myMPU6500 = MPU6500_WE(&wire2)        -> uses the TwoWire object wire2 / MPU6500_ADDR
+ * MPU6500_WE myMPU6500 = MPU6500_WE(&wire2, MPU6500_ADDR) -> all together
+ * Successfully tested with two I2C busses on an ESP32
+ */
+MPU6500_WE myMPU6500 = MPU6500_WE(MPU6500_ADDR);
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  if(!myMPU6500.init()){
+    Serial.println("MPU6500 does not respond");
+  }
+  else{
+    Serial.println("MPU6500 is connected");
+  }
+  Serial.println("Position you MPU6500 flat and don't move it - calibrating...");
+  delay(1000);
+  myMPU6500.autoOffsets();
+  Serial.println("Done!");
+  myMPU6500.enableGyrDLPF();
+  myMPU6500.setGyrDLPF(MPU6500_DLPF_6);
+  myMPU6500.setSampleRateDivider(5);
+  myMPU6500.setGyrRange(MPU6500_GYRO_RANGE_250);
+  myMPU6500.setAccRange(MPU6500_ACC_RANGE_2G);
+  myMPU6500.enableAccDLPF(true);
+  myMPU6500.setAccDLPF(MPU6500_DLPF_6);
+  delay(200);
+}
+
+void loop() {
+  xyzFloat gValue = myMPU6500.getGValues();
+  xyzFloat gyr = myMPU6500.getGyrValues();
+  float temp = myMPU6500.getTemperature();
+  float resultantG = myMPU6500.getResultantG(gValue);
+  Serial.println("Gyroscope data in degrees/s: ");
+  Serial.print(gyr.x);
+  Serial.print("   ");
+  Serial.print(gyr.y);
+  Serial.print("   ");
+  Serial.println(gyr.z);
+  Serial.print("Temperature in °C: ");
+  Serial.println(temp);
+  Serial.println("********************************************");
+  delay(1000);
+}
